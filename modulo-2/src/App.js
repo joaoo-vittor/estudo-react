441ef77547634449b2a7dfab3fb2b8d496884802
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-07-18 22:31:59 -0300

    study: Hook's in react

--- modulo-2/src/App.js
+++ modulo-2/src/App.js
@@ -1,43 +1,84 @@
-import logo from './logo.svg';
+import P from 'prop-types';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState, useMemo, useRef } from 'react';
+
+const Post = ({ post, handleClick }) => {
+  console.log('filho renderizou');
+
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+  handleClick: P.func,
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'App-logo-reverse' : '';
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(null);
 
-  const handleClickReverse = () => {
-    setReverse(!reverse);
-  };
+  console.log('pai renderizou');
 
-  const handleClickUp = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-  };
+  // ComponentDidMount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((resp) => {
+        return setPosts(resp);
+      });
+  }, []);
 
-  const handleClickDown = () => {
-    setCounter((counter) => counter - 1);
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  });
+
+  const handleClick = (value) => {
+    setValue(value);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+      <h1>Renderizou: {contador.current}</h1>
 
-        <h1>Contador: {counter}</h1>
+      <p>
+        <input
+          ref={input}
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
 
-        <button type="button" onClick={handleClickUp}>
-          Up
-        </button>
-        <br />
-        <button type="button" onClick={handleClickDown}>
-          Down
-        </button>
-        <br />
-        <button type="button" onClick={handleClickReverse}>
-          Reverse
-        </button>
-      </header>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post post={post} key={post.id} handleClick={handleClick} />;
+          })
+        );
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Ainda não exitem posts.</p>}
     </div>
   );
 }
