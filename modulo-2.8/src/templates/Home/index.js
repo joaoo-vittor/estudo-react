36549cc3084a88fe2ab752a291983f09321ed607
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-07-31 12:55:16 -0300

    study: Padrão Compound Components, React.Children, React.cloneElement e Context API

--- modulo-2.8/src/templates/Home/index.js
+++ modulo-2.8/src/templates/Home/index.js
@@ -1,3 +1,55 @@
+import { useState, Children, cloneElement, createContext, useContext } from 'react';
+
+const s = {
+  style: { fontSize: '60px' },
+};
+
+// const Parent = ({ children }) => {
+//   return Children.map(children, (child) => {
+//     const newChild = cloneElement(child, { ...s });
+//     return newChild;
+//   });
+// };
+
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
+  // return Children.map(children, (child) => {
+  //   const newChild = cloneElement(child, {
+  //     isOn,
+  //     onTurn,
+  //   });
+
+  //   return newChild;
+  // });
+};
+
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+  return <button onClick={onTurn}>Turn {isOn ? 'OFF' : 'ON'}</button>;
+};
+
 export const Home = () => {
-  return <div style={{ fontSize: '60px' }}>Oi</div>;
+  return (
+    <TurnOnOff>
+      <div>
+        <TurnedOn>Coisas do On.</TurnedOn>
+        <TurnedOff>Coisas do Off.</TurnedOff>
+        <TurnButton />
+      </div>
+    </TurnOnOff>
+  );
 };
