5b53fb77f26bc2b241acbd38eabbdd4a0fab897f
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-07-31 13:20:10 -0300

    study: React.lazy - Suspense e divisão de código (Code-Splitting)

--- modulo-2.9/src/templates/Home/index.js
+++ modulo-2.9/src/templates/Home/index.js
@@ -1,3 +1,23 @@
+import React, { useState, Suspense } from 'react';
+// import { LazyComponent } from './lazy_component';
+
+const laodComponent = () => {
+  console.log('Componente carregando!!!');
+  return import('./lazy_component');
+};
+const LazyComponent = React.lazy(laodComponent);
+
 export const Home = () => {
-  return <div style={{ fontSize: '60px' }}>Oi</div>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <p>
+        <button onMouseOver={laodComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'On' : 'Off'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Loading...</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
+  );
 };
