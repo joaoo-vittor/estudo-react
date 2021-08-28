2e20e2c158ae2433402e870068d33b6241ba1001
joaoo-vittor <joaoo.vittor007@gmail.com>
2021-08-27 23:18:25 -0300

    alter: app.test.jsx

--- projeto-1/src/templates/App/App.test.jsx
+++ projeto-1/src/templates/App/App.test.jsx
@@ -4,16 +4,8 @@ import { renderTheme } from '../../styles/renderTheme';
 import { theme } from '../../styles/theme';
 import { Home } from '.';
 
-test('renders learn react link', () => {
-  renderTheme(<Home />);
-
-  const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondaryBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-
-  expect(headingContainer).toHaveStyleRule('background', 'blue');
-
-  expect(headingContainer).toMatchSnapshot();
 });
